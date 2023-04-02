45dcd33f415205a0c2d3c29fb86fa63276f6c5a8
Victor Blanco <12vblanco@gmail.com>
2023-04-02 22:18:31 +0100

    icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>L.M. Douglas</h1>
+      <img src={logo} alt="lorraine's logo"></img>
       <div className="card">
-        <p>Coming soon!!</p>
+        <h1>Coming soon!!</h1>
       </div>
     </div>
   );
