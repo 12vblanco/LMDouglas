4ac246f1edaa921958bacf317bfa22301b7280de
Victor Blanco <12vblanco@gmail.com>
2023-09-24 19:41:04 +0100

    conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,68 @@
 import React from "react";
 import CookieConsent from "react-cookie-consent";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navigation/Navbar";
+import Bio from "./components/pages/Bio";
 import Home from "./components/pages/Home";
+import Terms from "./components/terms/Terms";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <CookieConsent
-        containerClasses="cookie_container"
+        buttonText="Accept"
+        // containerClasses="cookie_container"
         style={{
-          background: "rgba(2, 60, 113, 0.9)",
+          // fontFamily: "Montserrat",
+          background: "var(--dark)",
           fontWeight: "500",
-          height: "10vh",
+          height: "fit-content",
           alignItems: "center",
-          fontSize: "18px",
+          fontSize: "17px",
         }}
         buttonStyle={{
           borderRadius: "50px",
-          padding: "20px 32px",
+          padding: "14px 26px",
+          marginRight: "16px",
           fontSize: "18px",
           fontWeight: "700",
-          background: "white",
+          background: "var(--gold)",
           color: "rgb(2, 60, 113)",
         }}
-        contentStyle={{ textAlign: "left", marginBottom: "12px" }}
+        contentStyle={{
+          textAlign: "left",
+          marginBottom: "18px",
+          padding: "12px",
+        }}
       >
-        This website uses cookies and third party software to monitor traffic
-        anonymously and improve user experience. For more info read my{" "}
+        {" "}
+        I use cookies in my website. By clicking “Accept”, you consent to the
+        use of these the cookies. To learn more about how we use cookies and
+        your rights, please read our
         <Span>
-          <a href="/terms" aria-label="Visit Victor Blanco's Terms Page">
-            terms & conditions
-          </a>
+          <a href="/terms-of-service"> Terms of Service</a>
         </Span>
+        .
       </CookieConsent>
-      <BrowserRouter>
-        <Navbar />
-        <Home />
-        <Footer />
-      </BrowserRouter>
-    </>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/home#contact" element={<Home />} />
+        <Route path="/bio" element={<Bio />} />
+        <Route path="/terms" element={<Terms />} />
+        {/* <Route path="/success" element={<Success />} /> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
+
 const Span = styled.span`
   a {
-    color: white;
+    color: var(--gold);
     text-decoration: underline;
   }
 `;
