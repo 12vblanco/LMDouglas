90a78d79223d6847954e32740a7866f8191ca43b
Victor Blanco <12vblanco@gmail.com>
2024-09-16 18:52:50 +0100

    buy

--- src/components/hero/BookBlock2.jsx
+++ src/components/hero/BookBlock2.jsx
@@ -11,7 +11,7 @@ const BookBlock2 = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Pre-ORDER
+          Buy on
           <br />
           AMAZON.COM
         </Button>
@@ -20,7 +20,7 @@ const BookBlock2 = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Pre-ORDER
+          Buy on
           <br />
           AMAZON.UK
         </Button>
