d8f021a29600ae135603d18907bd1429824edccc
Victor Blanco <12vblanco@gmail.com>
2025-02-03 09:52:04 +0800

    fixt book 1 text

--- src/components/hero/data.js
+++ src/components/hero/data.js
@@ -3,6 +3,8 @@ const booksData = [
   {
     id: 1,
     title: "Gharantia's Guardian",
+    description:
+      "The devastating gods’ war that ravaged the other side of the world for centuries is coming to Gharantia. Its rulers, Princess Melane and Criterious, the Guardian, stand as sentinels, safeguarding their cherished realm. \n\nAs the Queen’s protective mist—the Bleak—rolls away after another winter, prophetic dreams haunt Melane and mysterious strangers converge on their city. \n\nCriterious must unravel the truth behind the war before it destroys them all. Who will stand with Gharantia against the coming tide? \n\nIn this epic fantasy adventure alliances will be forged, loyalties will waver, and destinies collide.",
     image: {
       webp: "../../assets/imgs/book_coverX1.webp",
       png: "../../assets/imgs/book_coverX1.png",
@@ -25,6 +27,8 @@ const booksData = [
   {
     id: 2,
     title: "Placeholder Title",
+    description:
+      "The gods' war draws near, and Lord Xuren threatens to unleash his relentless evil to obtain an artefact of unfathomable power. \n\nUnforeseen obstacles hinder Criterious and Princess Melane’s preparations for the conflict. Criterious’s transformation, brought on by his half-bond to the Synatha, has driven a wedge between him and his wife. Caught in a tangled web of enchanted deceit, they find themselves battling against fate itself. \n\nIn this gripping sequel, relationships are tested, new enemies emerge, and the true cost of war is revealed. \n\nAre Gharantia’s defenders truly ready to face their most formidable adversary yet?",
     image: {
       webp: "../../assets/imgs/placeholder.webp",
       png: "../../assets/imgs/placeholder.png",
